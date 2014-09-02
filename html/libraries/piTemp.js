d2232d3586e84e2e088491d96ec196547e05930e
FiesoDuck <FiesoDuck@users.noreply.github.com>
2014-09-02 13:20:48 +0200

    stuff 2.0

--- html/libraries/piTemp.js
+++ html/libraries/piTemp.js
@@ -3,6 +3,7 @@ var timevar = 1000;					// verzögerung in MS mit der ausgeben() ausgefuehrt wird
 var graph = {};						// array mit graphen
 var schalter = 0;					// on / off für Status
 var focusvar = 1;					// browserfenster im focus ja/nein
+var limit = 0;
 
 // browserfenster im focus 1 sonst 0
 window.onblur= function() {
@@ -36,28 +37,34 @@ $.ajax({
 
 // ausgabe von graph und daten, rekursiv 
 function ausgeben () {
-if (schalter == "on" && focusvar ==1) {			// abhaengig ob fenster im focus ist und "on"
+if (schalter == "on" && focusvar ==1) {				// abhaengig ob fenster im focus ist und "on"
 	$('#status').text("on");
-	if (moehre == 16) {							// wenn 8 mal daten ausgegeben worden sind -> neue daten aus getData() holen
+	if (moehre == 16) {											// wenn 8 mal daten ausgegeben worden sind -> neue daten aus getData() holen
 		moehre = 0;
 		getData();
 		}
 	else {
 		graph[moehre].value = celsius[moehre];   	// graph[aktueller zählervar wert] mit den daten aus dem celsius array fuellen
-		$('#data'+moehre).text(celsius[moehre]); 	// text feld (span tag) mit wert fuellen
-		graph[moehre].grow();						// graph aktualisieren mit grow animation
-		moehre++;									// zählervar auf naechste graph[id] setzen
-		setTimeout(ausgeben, timevar);				// verzoegern um graph zeit für grow tz geben
+		console.log(celsius[moehre], limit);				
+		if (limit != 0 && celsius[moehre] >= limit) { 		// Limit warnung
+		alert("Limit ueberschritten \n" + "Soll: " + limit + "\nIst: " +celsius[moehre]);
+		$('#thi'+moehre).css("background-color","red");
+		schalter="off";
+		};
+		$('#data'+moehre).text(celsius[moehre]); 		// text feld (span tag) mit wert fuellen
+		graph[moehre].grow();										// graph aktualisieren mit grow animation
+		moehre++;															// zählervar auf naechste graph[id] setzen
+		setTimeout(ausgeben, timevar);					// verzoegern um graph zeit für grow tz geben
 	}
 }
-else if (schalter == "off") {						// ausgabe stoppen
+else if (schalter == "off") {											// ausgabe stoppen
 	$('#status').text("off");
 	moehre = 0;
 	}
 else if (schalter == "error") {
 	$('#status').text("ERROR");
 	moehre = 0;	
-}
+	}
 }
 // user eingabe nutzen um ausgabe zu steuern
 function choose(choice){							
@@ -70,6 +77,13 @@ var mint=document.getElementById('mint').value;
 var maxt=document.getElementById('maxt').value;
 graph[0]  = new RGraph.VProgress({id: 'g0',  min: Number(mint), max: Number(maxt), value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
 
+}
+
+function limits() {
+var teste = document.getElementById('limitt').type;
+limit = document.getElementById('limitt').value; 
+if (limit == "") {alert("Bitte Zahl eingeben!")}
+else {console.log(limit); $('#limit').text(limit)}
 }
 // graphen beim Seitenaufbau einmalig zeichnen
 // ID von Canvas, min, max, value 0 -> startwert, gutter abstand von graph zu canvas (?), labels seite und wieviele, farbe -> draw!
