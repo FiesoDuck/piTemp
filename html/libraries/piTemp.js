17bb52497eafafab3c748ec73f13310de68b77c0
FiesoDuck <FiesoDuck@users.noreply.github.com>
2014-09-15 06:44:39 +0200

    working

--- html/libraries/piTemp.js
+++ html/libraries/piTemp.js
@@ -1,11 +1,14 @@
-var moehre = 0;						// zaehler var. läuft von 1-8 und stellt geräte ID bzw graph ID dar
-var timevar = 1000;					// verzögerung in MS mit der ausgeben() ausgefuehrt wird. -> wenn zu schnell kann ausgabe ruckeln
-var graph = {};						// array mit graphen
-var schalter = 0;					// on / off für Status
-var focusvar = 1;					// browserfenster im focus ja/nein
+var moehre = 0;														// zaehler var. läuft von 1-8 und stellt geräte ID bzw graph ID dar
+var timevar = 1000;												// verzögerung in MS mit der ausgeben() ausgefuehrt wird. -> wenn zu schnell kann ausgabe ruckeln
+var graph = {};															// array mit graphen
+var schalter = 0;														// on / off für Status
+var focusvar = 1;													// browserfenster im focus ja/nein
 var limit = 0;
 var limitwar = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 var cords = [40,190, 2];											// skala 0 punkte, 190 - 166 ist obere grenze für höhe -> 24, Höhe / Max Skala * Grenze  //- cords[1] / graph[moehre].max * limit ,
+var tmax = 120;														// max wert für graph temp
+var qmax = 40; 														// max wert für graph durchfluss
+var limitcolor = "red";												// farbe limit
 
 // browserfenster im focus 1 sonst 0
 window.onblur= function() {
@@ -24,11 +27,11 @@ var data;
 var jsonurl = './tnow.json';
 $.ajax({
 	type: "GET",
-	url: jsonurl,						// Daten-Stream url
-	data: data,							// Variable für json Container
+	url: jsonurl,																// Daten-Stream url
+	data: data,																// Variable für json Container
 	async: true,						
 	dataType: "json",
-	success: function(data){			// nur ausführen wenn getJson (hier ajax) erfolgreich war sonst zu error:
+	success: function(data){										// nur ausführen wenn getJson (hier ajax) erfolgreich war sonst zu error:
 	console.log("get ./tnow.json");		
 	celsius = data.temperature_record;
 	grenze();
@@ -42,30 +45,52 @@ var data;
 var jsonurl = './limitsnow.json';
 $.ajax({
 	type: "GET",
-	url: jsonurl,						// Daten-Stream url
-	data: data,							// Variable für json Container
+	url: jsonurl,																	// Daten-Stream url
+	data: data,																	// Variable für json Container
 	async: true,						
 	dataType: "json",
-	success: function(data){			// nur ausführen wenn getJson (hier ajax) erfolgreich war sonst zu error:
+	success: function(data){											// nur ausführen wenn getJson (hier ajax) erfolgreich war sonst zu error:
 	console.log("get ./limitsnow.json");		
-	limits = data.dev0.max;
-	console.log(limits);
-	graph[16] = new RGraph.Drawing.Rect({id: 'g0', x: cords[0] , 	y: cords[1] - 166 / graph[0].max * data.dev0.max, width: 38, height: 2}).draw();	
-	graph[17] = new RGraph.Drawing.Rect({id: 'g0', x: cords[0] , 	y: cords[1] - 166 / graph[0].max * data.dev0.min, width: 38, height: 2}).draw();
-	graph[18] = new RGraph.Drawing.Rect({id: 'g1', x: cords[2] , 	y: cords[1] - 166 / graph[1].max * data.dev1.max, width: 38, height: 2}).draw();
-	graph[19] = new RGraph.Drawing.Rect({id: 'g1', x: cords[2] , 	y: cords[1] - 166 / graph[1].max * data.dev1.min, width: 38, height: 2}).draw();
-	graph[20] = new RGraph.Drawing.Rect({id: 'g2', x: cords[0] , 	y: cords[1] - 166 / graph[2].max * data.dev2.max, width: 38, height: 2}).draw();
-	graph[21] = new RGraph.Drawing.Rect({id: 'g2', x: cords[0] , 	y: cords[1] - 166 / graph[2].max * data.dev2.min, width: 38, height: 2}).draw();
-	graph[22] = new RGraph.Drawing.Rect({id: 'g3', x: cords[2] , 	y: cords[1] - 166 / graph[3].max * data.dev3.max, width: 38, height: 2}).draw();
-	graph[23] = new RGraph.Drawing.Rect({id: 'g3', x: cords[2] , 	y: cords[1] - 166 / graph[3].max * data.dev3.min, width: 38, height: 2}).draw();
-	graph[24] = new RGraph.Drawing.Rect({id: 'g4', x: cords[0] , 	y: cords[1] - 166 / graph[4].max * data.dev4.max, width: 38, height: 2}).draw();
-	graph[25] = new RGraph.Drawing.Rect({id: 'g4', x: cords[0] , 	y: cords[1] - 166 / graph[4].max * data.dev4.min, width: 38, height: 2}).draw();
-	graph[26] = new RGraph.Drawing.Rect({id: 'g5', x: cords[2] , 	y: cords[1] - 166 / graph[5].max * data.dev5.max, width: 38, height: 2}).draw();
-	graph[27] = new RGraph.Drawing.Rect({id: 'g5', x: cords[2] , 	y: cords[1] - 166 / graph[5].max * data.dev5.min, width: 38, height: 2}).draw();
-	graph[28] = new RGraph.Drawing.Rect({id: 'g6', x: cords[0] , 	y: cords[1] - 166 / graph[6].max * data.dev6.max, width: 38, height: 2}).draw();
-	graph[29] = new RGraph.Drawing.Rect({id: 'g6', x: cords[0] , 	y: cords[1] - 166 / graph[6].max * data.dev6.min, width: 38, height: 2}).draw();
-	graph[30] = new RGraph.Drawing.Rect({id: 'g7', x: cords[2] , 	y: cords[1] - 166 / graph[7].max * data.dev7.max, width: 38, height: 2}).draw();
-	graph[31] = new RGraph.Drawing.Rect({id: 'g7', x: cords[2] , 	y: cords[1] - 166 / graph[7].max * data.dev7.min, width: 38, height: 2}).draw();	
+	for (var key in data) {
+		var obj = data[key]
+		console.log(obj.max);
+		if (obj.max > tmax) {
+			key.obj.max = tmax;
+			}
+		}
+		
+	graph[16] = new RGraph.Drawing.Rect({id: 'g0', x: cords[0] , 	y: cords[1] - 166 / graph[0].max * data.dev0.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[17] = new RGraph.Drawing.Rect({id: 'g0', x: cords[0] , 	y: cords[1] - 166 / graph[0].max * data.dev0.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[18] = new RGraph.Drawing.Rect({id: 'g1', x: cords[2] , 	y: cords[1] - 166 / graph[1].max * data.dev1.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[19] = new RGraph.Drawing.Rect({id: 'g1', x: cords[2] , 	y: cords[1] - 166 / graph[1].max * data.dev1.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[20] = new RGraph.Drawing.Rect({id: 'g2', x: cords[0] , 	y: cords[1] - 166 / graph[2].max * data.dev2.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[21] = new RGraph.Drawing.Rect({id: 'g2', x: cords[0] , 	y: cords[1] - 166 / graph[2].max * data.dev2.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[22] = new RGraph.Drawing.Rect({id: 'g3', x: cords[2] , 	y: cords[1] - 166 / graph[3].max * data.dev3.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[23] = new RGraph.Drawing.Rect({id: 'g3', x: cords[2] , 	y: cords[1] - 166 / graph[3].max * data.dev3.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[24] = new RGraph.Drawing.Rect({id: 'g4', x: cords[0] , 	y: cords[1] - 166 / graph[4].max * data.dev4.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[25] = new RGraph.Drawing.Rect({id: 'g4', x: cords[0] , 	y: cords[1] - 166 / graph[4].max * data.dev4.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[26] = new RGraph.Drawing.Rect({id: 'g5', x: cords[2] , 	y: cords[1] - 166 / graph[5].max * data.dev5.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[27] = new RGraph.Drawing.Rect({id: 'g5', x: cords[2] , 	y: cords[1] - 166 / graph[5].max * data.dev5.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[28] = new RGraph.Drawing.Rect({id: 'g6', x: cords[0] , 	y: cords[1] - 166 / graph[6].max * data.dev6.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[29] = new RGraph.Drawing.Rect({id: 'g6', x: cords[0] , 	y: cords[1] - 166 / graph[6].max * data.dev6.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[30] = new RGraph.Drawing.Rect({id: 'g7', x: cords[2] , 	y: cords[1] - 166 / graph[7].max * data.dev7.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[31] = new RGraph.Drawing.Rect({id: 'g7', x: cords[2] , 	y: cords[1] - 166 / graph[7].max * data.dev7.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[32] = new RGraph.Drawing.Rect({id: 'g8', x: cords[0] , 	y: cords[1] - 166 / graph[8].max * data.dev8.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[33] = new RGraph.Drawing.Rect({id: 'g8', x: cords[0] , 	y: cords[1] - 166 / graph[8].max * data.dev8.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();
+	graph[34] = new RGraph.Drawing.Rect({id: 'g9', x: cords[2] , 	y: cords[1] - 166 / graph[9].max * data.dev9.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[35] = new RGraph.Drawing.Rect({id: 'g9', x: cords[2] , 	y: cords[1] - 166 / graph[9].max * data.dev9.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[36] = new RGraph.Drawing.Rect({id: 'g10', x: cords[0] , 	y: cords[1] - 166 / graph[10].max * data.dev10.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[37] = new RGraph.Drawing.Rect({id: 'g10', x: cords[0] , 	y: cords[1] - 166 / graph[10].max * data.dev10.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[38] = new RGraph.Drawing.Rect({id: 'g11', x: cords[2] , 	y: cords[1] - 166 / graph[11].max * data.dev11.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[39] = new RGraph.Drawing.Rect({id: 'g11', x: cords[2] , 	y: cords[1] - 166 / graph[11].max * data.dev11.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[40] = new RGraph.Drawing.Rect({id: 'g12', x: cords[0] , 	y: cords[1] - 166 / graph[12].max * data.dev12.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[41] = new RGraph.Drawing.Rect({id: 'g12', x: cords[0] , 	y: cords[1] - 166 / graph[12].max * data.dev12.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[42] = new RGraph.Drawing.Rect({id: 'g13', x: cords[2] , 	y: cords[1] - 166 / graph[13].max * data.dev13.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[43] = new RGraph.Drawing.Rect({id: 'g13', x: cords[2] , 	y: cords[1] - 166 / graph[13].max * data.dev13.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();		
+	graph[44] = new RGraph.Drawing.Rect({id: 'g14', x: cords[0] , 	y: cords[1] - 166 / graph[14].max * data.dev14.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[45] = new RGraph.Drawing.Rect({id: 'g14', x: cords[0] , 	y: cords[1] - 166 / graph[14].max * data.dev14.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[46] = new RGraph.Drawing.Rect({id: 'g15', x: cords[2] , 	y: cords[1] - 166 / graph[15].max * data.dev15.max, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();	
+	graph[47] = new RGraph.Drawing.Rect({id: 'g15', x: cords[2] , 	y: cords[1] - 166 / graph[15].max * data.dev15.min, width: 38, height: 2, options:{fillstyle:limitcolor}}).draw();			
 	},
 	error: function(){alert('Der Server antwortet nicht!'); schalter = "error";}
 	});	
@@ -121,7 +146,6 @@ getData();
 }
 
 function tester() {
-
 RGraph.ObjectRegistry.Remove(graph[16]);
 graph[16] = new RGraph.Drawing.Rect({id: 'g0', x: cords[0] , 	y: cords[1] -100 , width: 38, height: 30});
 RGraph.redraw();
@@ -151,20 +175,20 @@ $('#limit').text(limit);
 // graphen beim Seitenaufbau einmalig zeichnen
 // ID von Canvas, min, max, value 0 -> startwert, gutter abstand von graph zu canvas (?), labels seite und wieviele, farbe -> draw!
 function initGraph (json) {
-graph[0]  = new RGraph.VProgress({id: 'g0',  min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[1]  = new RGraph.VProgress({id: 'g1',  min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
-graph[2]  = new RGraph.VProgress({id: 'g2',  min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[3]  = new RGraph.VProgress({id: 'g3',  min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
-graph[4]  = new RGraph.VProgress({id: 'g4',  min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[5]  = new RGraph.VProgress({id: 'g5',  min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
-graph[6]  = new RGraph.VProgress({id: 'g6',  min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[7]  = new RGraph.VProgress({id: 'g7',  min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
-graph[8]  = new RGraph.VProgress({id: 'g8',  min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[9]  = new RGraph.VProgress({id: 'g9',  min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
-graph[10] = new RGraph.VProgress({id: 'g10', min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[11] = new RGraph.VProgress({id: 'g11', min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
-graph[12] = new RGraph.VProgress({id: 'g12', min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[13] = new RGraph.VProgress({id: 'g13', min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
-graph[14] = new RGraph.VProgress({id: 'g14', min: 0, max: 120, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
-graph[15] = new RGraph.VProgress({id: 'g15', min: 0, max: 40,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[0]  = new RGraph.VProgress({id: 'g0',  min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[1]  = new RGraph.VProgress({id: 'g1',  min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[2]  = new RGraph.VProgress({id: 'g2',  min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[3]  = new RGraph.VProgress({id: 'g3',  min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[4]  = new RGraph.VProgress({id: 'g4',  min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[5]  = new RGraph.VProgress({id: 'g5',  min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[6]  = new RGraph.VProgress({id: 'g6',  min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[7]  = new RGraph.VProgress({id: 'g7',  min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[8]  = new RGraph.VProgress({id: 'g8',  min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[9]  = new RGraph.VProgress({id: 'g9',  min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[10] = new RGraph.VProgress({id: 'g10', min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[11] = new RGraph.VProgress({id: 'g11', min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[12] = new RGraph.VProgress({id: 'g12', min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[13] = new RGraph.VProgress({id: 'g13', min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
+graph[14] = new RGraph.VProgress({id: 'g14', min: 0, max: tmax, value: 0, options: {scale: {decimals: 1}, gutter: {right: 2, left:  40, bottom: 10}, labels: {position: "left" , count: 5}, colors:['#5A8F29']}}).draw();
+graph[15] = new RGraph.VProgress({id: 'g15', min: 0, max: qmax,  value: 0, options: {scale: {decimals: 1}, gutter: {left:  2, right: 40, bottom: 10}, labels: {position: "right", count: 5}, colors:['#3C7DC4']}}).draw();
 }
